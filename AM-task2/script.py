3989295aafb37aa7b79cba3b5430c29971be56b9
antoinemeyer5 <antoine.meyer54@gmail.com>
2026-07-07 12:38:21 +0200

    AM: improve debug infos

--- AM-task2/script.py
+++ AM-task2/script.py
@@ -54,16 +54,22 @@ if __name__ == "__main__":
   path1 = sys.argv[1]
   path2 = sys.argv[2]
 
+  print(f"Between *{path1.split('/')[-1]}* and *{path2.split('/')[-1]}*:\n")
+
   configContent1 = readFile(path1)
   configContent2 = readFile(path2)
 
   a = normalize_deck(configContent1)
   b = normalize_deck(configContent2)
 
-  print(f"Between *{path1.split('/')[-1]}* and *{path2.split('/')[-1]}*:")
-  print(f"Jaccard distance (0 same - 1 different): {jaccard_distance(a, b)}")
+  print(f"Normalization a: {a}")
+  print(f"Normalization b: {b}")
+  print(f"Jaccard distance (0 same - 1 different): {jaccard_distance(a, b)}\n")
 
   A = simhash(a)
   B = simhash(b)
+
+  print(f"Hash A: {A}")
+  print(f"Hash B: {B}")
 
   print(f"Hamming distance (0 same - >16 different): {hamming_distance(A, B)}")
